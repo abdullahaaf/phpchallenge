2559ae6270439263081750efea618c56d09209dc
abdullahaaf <abdullahaaf1@gmail.com>
2018-10-10 16:25:17 +0700

    add docs for abstract class

--- Mobil.php
+++ Mobil.php
@@ -1,4 +1,9 @@
 <?php
+    /**
+     * abstract class mobil,
+     * abstract ini harus berisi
+     * abstract funtion
+     */
     abstract class Mobil
     {
         abstract function brand($brand);
